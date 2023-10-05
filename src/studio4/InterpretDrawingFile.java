4c3141896c5837be0836383afe78e3ab2dbe6fa3
michaellerman <michaellerman@localhost>
2023-10-05 11:09:41 -0500

    Hello

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,7 @@
 package studio4;
 
+import java.awt.Color;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +22,58 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+//		File f = new File("resources/rectangle_pink_filled.txt");
+//		Scanner in = new Scanner(f);
+		
+		String shape = in.next();
+		int redValue = in.nextInt();
+		int greenValue = in.nextInt();
+		int blueValue = in.nextInt();
+		boolean filled = in.nextBoolean();
+
+		Color color = new Color (redValue, greenValue, blueValue);
+		StdDraw.setPenColor(color);
+
+		if (shape.equals("rectangle")) {
+			double xValue = in.nextDouble();
+			double yValue = in.nextDouble();
+			double widthValue = in.nextDouble();
+			double heightValue = in.nextDouble();
+			if (filled == true) {
+				StdDraw.filledRectangle(xValue, yValue, widthValue, heightValue);
+			}
+			else {
+				StdDraw.rectangle(xValue, yValue, widthValue, heightValue);
+			}
+		}
+		else if (shape.equals("ellipse")) {
+			double xValue = in.nextDouble();
+			double yValue = in.nextDouble();
+			double widthValue = in.nextDouble();
+			double heightValue = in.nextDouble();
+			if (filled == true) {
+				StdDraw.filledEllipse(xValue, yValue, widthValue, heightValue);
+			}
+			else {
+				StdDraw.ellipse(xValue, yValue, widthValue, heightValue);
+			}
+		}
+		else if (shape.equals("triangle")) {
+			double xValue1 = in.nextDouble();
+			double yValue1 = in.nextDouble();
+			double xValue2 = in.nextDouble();
+			double yValue2 = in.nextDouble();
+			double xValue3 = in.nextDouble();
+			double yValue3 = in.nextDouble();
+			
+			double[] xValues = {xValue1, xValue2, xValue3};
+			double[] yValues = {yValue1, yValue2, yValue3};
+			if (filled == true) {
+				StdDraw.filledPolygon(xValues,yValues);
+			}
+			else {
+				StdDraw.polygon(xValues, yValues);
+			}
+		}
 	}
 }
